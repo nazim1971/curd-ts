a2bd3283cd97e0b016a58ce07f24c4f7e76569b6
nazim1971 <md.nazimuddinaj@gmail.com>
2024-09-21 03:46:57 +0600

    update db

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-let i = 10
-console.log(i);
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 5000;
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log(`My server is running on port ${PORT} `);
+})
