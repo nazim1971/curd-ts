5a34ad19699eddad3451fd661abe004146afc77f
nazim1971 <md.nazimuddinaj@gmail.com>
2024-09-21 04:06:06 +0600

    db successfully connected

---
+++ src/db/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+
+
+
+const uri  = process.env.MY_DB;
+
+if (!uri) {
+    throw new Error("MongoDB URI is not defined in the environment variables");
+  }
+
+async function connectDB() {
+    try {
+      await mongoose.connect(uri!); // get error here
+      console.log("Connected to MongoDB successfully!");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1); 
+    }
+  }
+  
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import express from "express";
-import mongoose from "mongoose";
+import connectDB from "./db/db";
+
 
 const app = express();
 const PORT = 5000;
+
+//middlewire
 app.use(express.json());
 
-app.listen(PORT, ()=>{
-    console.log(`My server is running on port ${PORT} `);
-})
+
+
+// Define a function to start the server
+const startServer = async () => {
+    try {
+      // Connect to the database
+      await connectDB(); // Await the database connection
+  
+      // Start the server after successful DB connection
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error("Failed to connect to the database:", error);
+      process.exit(1); // Exit the process if the connection fails
+    }
+  };
+  
+  // Call the function to start the server
+  startServer();
