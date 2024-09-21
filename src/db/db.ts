42558a2cfd5a1043e19a2c001a6b780837011ce4
nazim1971 <md.nazimuddinaj@gmail.com>
2024-09-21 15:59:05 +0600

    undo

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,15 +4,15 @@ dotenv.config();
 
 
 
-const uri  = process.env.MY_DB as string;
+const uri  = process.env.MY_DB;
 
-if (uri) {
+if (!uri) {
     throw new Error("MongoDB URI is not defined in the environment variables");
   }
 
 async function connectDB() {
     try {
-      await mongoose.connect(uri); // get error here
+      await mongoose.connect(uri!); // get error here
       console.log("Connected to MongoDB successfully!");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
