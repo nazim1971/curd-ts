855a327dc01a0cee661ec818d16bfeda9caa787b
nazim1971 <md.nazimuddinaj@gmail.com>
2024-09-21 15:57:03 +0600

    update db

--- src/db/db.ts
+++ src/db/db.ts
@@ -16,7 +16,7 @@ async function connectDB() {
       console.log("Connected to MongoDB successfully!");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
-      
+      process.exit(1); 
     }
   }
   
